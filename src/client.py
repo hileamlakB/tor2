cb89e3bead27c8297fc78d33a270545f30fd6d97
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-04-22 12:21:22 -0400

    client scaffolding

--- src/client.py
+++ src/client.py
@@ -1,7 +1,12 @@
+from __future__ import print_function
+
 import grpc
 import tor_pb2
 import tor_pb2_grpc
 
+import logging
+import threading
+import cmd
 
 def get_relay_nodes():
     channel = grpc.insecure_channel("localhost:50051")
@@ -19,3 +24,26 @@ def send_message(message, relay_nodes):
 
 # implement client logic preferably
 # using the cmd module to create a command line interface
+class JTor_Client(cmd.Cmd):
+    prompt = "JTor> "
+
+    def __init__(self, port):
+        super(JTor_Client, self).__init__()
+
+        self.user_session_id = ""
+        self.channel = grpc.insecure_channel(f'localhost:{port}')
+        self.stub = tor_pb2_grpc.ClientStub(self.channel)
+
+        self.do_help("")
+
+    # Print various help messages
+    def do_help(self, arg):
+        print("Placeholder help message")
+        print(arg)
+
+        # HELP_SEND = 'send a url through the network'
+        # usage: send <url> <entry_node>
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    JTor_Client(2625).cmdloop()
