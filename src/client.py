d30ab27cc465f3a41ee1a6c20b839f61597af417
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-04-23 09:48:01 -0400

    implement getNode for client and directory server

--- src/client.py
+++ src/client.py
@@ -9,11 +9,16 @@ import threading
 import cmd
 
 def get_relay_nodes():
-    channel = grpc.insecure_channel("localhost:50051")
+    channel = grpc.insecure_channel("localhost:50051") # directory server lives here
     stub = tor_pb2_grpc.DirectoryServerStub(channel)
     response = stub.GetRelayNodes(tor_pb2.GetRelayNodesRequest())
     return response.relay_nodes
 
+# def send(self, entry_node_addr):
+#     channel = grpc.insecure_channel(f"localhost:{entry_node_addr}") # localhost entrynode
+#     stub = tor_pb2_grpc.RelayStub(channel)
+#     response = stub.GetRelayNodes(tor_pb2.ProcessMessage())
+#     return response.relay_nodes
 
 def send_message(message, relay_nodes):
     # Implement Key sharing with relays
@@ -38,7 +43,13 @@ class JTor_Client(cmd.Cmd):
 
     def do_getNode(self, arg):
         print("doing getnode")
-        pass
+        relay_nodes = get_relay_nodes()
+        if relay_nodes[0].node_type == tor_pb2.ENTRY:
+            self.relay_entry = relay_nodes[0]
+        if relay_nodes[1].node_type == tor_pb2.MIDDLE:
+            self.relay_middle = relay_nodes[1]
+        if relay_nodes[2].node_type == tor_pb2.EXIT:
+            self.relay_exit = relay_nodes[2]
 
     def do_send(self, arg):
         args = arg.split(" ")
@@ -48,8 +59,20 @@ class JTor_Client(cmd.Cmd):
         
         url = args[0]
 
+        if self.relay_entry is None:
+            print("Error: No entry node specified")
+            return
+        if self.relay_middle is None:
+            print("Error: No middle node specified")
+            return
+        if self.relay_exit is None:
+            print("Error: No exit node specified")
+            return
+        
+        # TODO: Validate the URL is nonempty
+
     def do_exit(self, arg):
-        print("doing exit")
+        print("Thanks for using JTor! Exiting...")
         quit()
 
     # Print various help messages
---
+++ src/tor_pb2.pyi
@@ -0,0 +1,70 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+ENTRY: NodeType
+EXIT: NodeType
+MIDDLE: NodeType
+NONNODE: NodeType
+
+class GetRelayNodesRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class GetRelayNodesResponse(_message.Message):
+    __slots__ = ["relay_nodes"]
+    RELAY_NODES_FIELD_NUMBER: _ClassVar[int]
+    relay_nodes: _containers.RepeatedCompositeFieldContainer[RelayNode]
+    def __init__(self, relay_nodes: _Optional[_Iterable[_Union[RelayNode, _Mapping]]] = ...) -> None: ...
+
+class ProcessMessageRequest(_message.Message):
+    __slots__ = ["encrypted_message", "next_relay_node"]
+    ENCRYPTED_MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    NEXT_RELAY_NODE_FIELD_NUMBER: _ClassVar[int]
+    encrypted_message: str
+    next_relay_node: RelayNode
+    def __init__(self, encrypted_message: _Optional[str] = ..., next_relay_node: _Optional[_Union[RelayNode, _Mapping]] = ...) -> None: ...
+
+class ProcessMessageResponse(_message.Message):
+    __slots__ = ["encrypted_message"]
+    ENCRYPTED_MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    encrypted_message: str
+    def __init__(self, encrypted_message: _Optional[str] = ...) -> None: ...
+
+class RelayNode(_message.Message):
+    __slots__ = ["address", "node_type", "public_key"]
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    NODE_TYPE_FIELD_NUMBER: _ClassVar[int]
+    PUBLIC_KEY_FIELD_NUMBER: _ClassVar[int]
+    address: str
+    node_type: NodeType
+    public_key: str
+    def __init__(self, address: _Optional[str] = ..., public_key: _Optional[str] = ..., node_type: _Optional[_Union[NodeType, str]] = ...) -> None: ...
+
+class RequestCircuitRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class RequestCircuitResponse(_message.Message):
+    __slots__ = ["relay_nodes"]
+    RELAY_NODES_FIELD_NUMBER: _ClassVar[int]
+    relay_nodes: _containers.RepeatedCompositeFieldContainer[RelayNode]
+    def __init__(self, relay_nodes: _Optional[_Iterable[_Union[RelayNode, _Mapping]]] = ...) -> None: ...
+
+class SendMessageRequest(_message.Message):
+    __slots__ = ["message"]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    def __init__(self, message: _Optional[str] = ...) -> None: ...
+
+class SendMessageResponse(_message.Message):
+    __slots__ = ["response"]
+    RESPONSE_FIELD_NUMBER: _ClassVar[int]
+    response: str
+    def __init__(self, response: _Optional[str] = ...) -> None: ...
+
+class NodeType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
